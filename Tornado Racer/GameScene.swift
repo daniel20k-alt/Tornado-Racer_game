6463e6f53133d8c5588792bae14cf6683a2bf221
Testing_author <testing_beta@mail.com>
2020-03-25 09:09:17 +0200

    Configured the "dustParticles" SKEmitterNode, "scoreLabel" as SKLabelNode and "player" as SKSpriteNode

--- Tornado Racer/GameScene.swift
+++ Tornado Racer/GameScene.swift
@@ -10,17 +10,43 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var dustParticles: SKEmitterNode!
+    var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
     
+    var score =  0 {
+    didSet {
+    scoreLabel.text = "Score: \(score)"
+    }
+    }
 
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-     
+        backgroundColor = .black
+        dustParticles = SKEmitterNode(fileNamed: "dustParticles")!
+        dustParticles.position = CGPoint(x: 1024, y: 284)
+        
+        dustParticles.advanceSimulationTime(10)  //makes the SMEmitter run for 10 seconds prior to opening
+        addChild(dustParticles)
+        dustParticles.zPosition = -1
+        
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
+        
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x:16, y:16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+        
+        score = 0
+
     }
-    
-    
-    
-    
+
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
